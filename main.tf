ddf3e6e1f5a868c7b7bd3eb6b66f9838896d71ef
matsuda-hiroki <matsuda-hiroki@users.noreply.github.com>
2020-01-28 16:57:22 +0900

    push

--- main.tf
+++ main.tf
@@ -15,3 +15,64 @@ resource "aws_vpc" "this" {
     Name = "tf-example-vpc"
   }
 }
+
+# InternetGateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = "${aws_vpc.this.id}"
+}
+
+# RouteTable
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.this.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.this.id}"
+  }
+
+  tags {
+    Name = "public"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public_a" {
+  vpc_id            = "${aws_vpc.this.id}"
+  cidr_block        = "10.1.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags {
+    Name = "public-a"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id            = "${aws_vpc.this.id}"
+  cidr_block        = "10.1.2.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags {
+    Name = "public-c"
+  }
+}
+
+resource "aws_subnet" "public_d" {
+  vpc_id            = "${aws_vpc.this.id}"
+  cidr_block        = "10.1.3.0/24"
+  availability_zone = "ap-northeast-1d"
+
+  tags {
+    Name = "public-d"
+  }
+}
+
+# SubnetRouteTableAssociation
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = "${aws_subnet.public_a.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = "${aws_subnet.public_c.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
